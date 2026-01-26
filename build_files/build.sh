2bbf5d3bccd3f5fecce05cc09d17476ed74b6f06
Tobias Zimpel <tobiaszimpel@me.com>
2026-01-26 16:54:27 +0100

    Add ujust commands to manage rEFInd

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,10 +2,13 @@
 
 set -ouex pipefail
 
-### Install packages
-dnf5 install -y podman-compose podman-docker
-
 # Remove Broadcom WiFi blacklist (T2 MBP)
 if [[ -f /usr/lib/modprobe.d/20-akmods.conf ]]; then
   sed -i '/^\s*blacklist\s\+brcmfmac\s*$/d' /usr/lib/modprobe.d/20-akmods.conf
 fi
+
+### Install packages
+dnf5 install -y podman-compose podman-docker
+
+### Add rEFInd management commands to ujust
+install -D -m 0644 /ctx/60-rEFInd-management.just /usr/share/ublue-os/just/60-rEFInd-management.just
